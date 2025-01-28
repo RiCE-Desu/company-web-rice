71649574c7e58c3bdb9ebf9e90f7f4a1d20aa928
Rais Fatihul Ihsan <rfix1210@gmail.com>
2025-01-28 14:51:58 +0700

    Fixing Errror

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Hero from './components/Hero';
 import Services from './components/Services';
 import About from './components/About';
 import Footer from './components/Footer';
-import Projects from './components/projects';
+import Projects from './components/ProjectsSection';
 
 const App = () => {
   return (
